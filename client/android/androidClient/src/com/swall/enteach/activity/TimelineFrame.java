0b7fbd8bc8ceaa2632aa1cec094fdae80b6acbe4
ip-mba <iptton@gmail.com>
2014-01-01 19:20:38 +0800

    commit for pull…

--- client/android/androidClient/src/com/swall/enteach/activity/TimelineFrame.java
+++ client/android/androidClient/src/com/swall/enteach/activity/TimelineFrame.java
@@ -1,18 +1,18 @@
-package com.example.SWall.activity;
+package com.swall.enteach.activity;
 
 import android.view.LayoutInflater;
 import android.view.View;
-import com.example.SWall.R;
+import com.swall.enteach.R;
 
 /**
  * Author: iptton
  * Date: 13-12-8
- * Time: 上午2:33
+ * Time: 上午2:39
  */
-public class ActiviesFrame extends TabFrame {
+public class TimelineFrame extends TabFrame {
     @Override
     public View onCreateView(LayoutInflater inflater) {
-        mView = inflater.inflate(R.layout.activities,null);
+        mView = inflater.inflate(R.layout.timeline,null);
         return mView;
     }
 }
